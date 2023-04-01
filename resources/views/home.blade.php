78b3c29ad44d33e1fd5373f919c6f3fa776c0fd6
charfeddine-naoufel <62994930+charfeddine-naoufel@users.noreply.github.com>
2023-04-01 14:45:38 +0200

    making front end

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,68 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="row d-flex justify-content-center align-items-center rows">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<div class="col-md-12">
 
-                    {{ __('You are logged in!') }}
+
+    <div class="card px-5">
+
+
+        <div class="text-center">
+
+            <h2 class=" mt-3">Tape URL and click ShortURL</h2>
+
+            <div class="mx-5">
+
+
+               <div class="input-group mb-3 mt-4">
+                  <input type="text" class="form-control" placeholder="Your URL ..." aria-label="Recipient's username" aria-describedby="button-addon2">
+                  <button class="btn btn-success border-rad" type="button" id="button-addon2">Short URL</button>
                 </div>
+                
+
             </div>
+            
         </div>
+        <div class=" table-responsive mt-5 ">
+                    <table class="table">
+                        <thead class="thead-dark" >
+                            <tr>
+                                <th>#</th>
+                                <th>url</th>
+                                <th>URL Shorted</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>www.google.com</td>
+                                <td>localhost/xvc5F</td>
+                                
+                                <td>
+                                    
+                                        <a href="#" ><i class="fa fa-edit text-success fa-2x"></i></a>
+                                        <a href="#" ><i class="fa fa-trash text-danger fa-2x"></i></a>
+                                   
+                                </td>
+                            </tr>
+                            
+                           
+                            
+                            
+                        </tbody>
+                    </table>
+                </div>
+        
     </div>
+    
+</div>
+
+
+
+
+</div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,16 +12,19 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    Naoufel Charfeddine
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -76,5 +79,8 @@
             @yield('content')
         </main>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
